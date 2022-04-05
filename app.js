f6c15a1b7f8860ac1cb708a429c0ea0b0cc75f58
Sajida <sajida.mohammad2093@gmail.com>
2022-04-05 13:39:28 -0500

    displayed the table

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var beerRouter= require('./routes/beer');
 var addmodsRouter= require('./routes/addmods');
+var selctorRouter= require('./routes/selector');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/beer', beerRouter);
 app.use('/addmods', addmodsRouter);
+app.use('/selector', selctorRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
